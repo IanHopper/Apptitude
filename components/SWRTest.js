7be7f9d5f88558d9ba7f5d9278209dc11098d350
Ian Hopper <iandthopper@gmail.com>
2022-03-10 11:55:00 -0600

    removed no-cors

--- components/SWRTest.js
+++ components/SWRTest.js
@@ -9,7 +9,7 @@ const local = "http://127.0.01:8000/api/v1/"
 const heroku = "https://ihop-blog-api.herokuapp.com/api/v1/"
 const url = dev ? local : heroku
 const token = 'ebe0b03ab76f86f3c2c892f38628ee79ba70d88b'
-const body = { method: 'GET', mode: 'no-cors', headers: { Authorization: `Token ${token}`}}
+const body = { method: 'GET', headers: { Authorization: `Token ${token}`}}
 
 const SWRTest = () => {
   const { data, error } = useSWR(url, async (url) => fetcher(url, body));
