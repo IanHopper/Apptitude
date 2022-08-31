b4299fe9c5ee7fe9d5fc416bd9c1a1c8c0c992f6
Ian Hopper <iandthopper@gmail.com>
2022-08-31 13:35:08 -0500

    added token refresh to (moved from Nav component)

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,49 @@ import TodoList from "../components/TodoList";
 import { useAppContext } from "../context/state";
 import { URL_ENDPOINT } from "../context/types";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const context = useAppContext();
-  const { updateTodos, auth, todos, projects } = context;
+  const { updateTodos, auth, todos, projects, loadUser, refreshJWT } = context;
+  
+  // Router used to check 
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  // Token refresh !! I think this gets interrupted on reloads !!
+  useEffect(() => {
+    let user = localStorage.getItem("user");
+    let token = localStorage.getItem("token");
+    let refresh = localStorage.getItem("refresh");
+    loadUser(user);
+    // Trigger token refresh by going to /
+    if (todos.length === 0 && token && refresh && pathname === "/") {
+      refreshJWT(refresh)
+    }
+    let interval = setInterval(() => {
+      if (token && refresh) {
+        refreshJWT(refresh);
+      }
+    }, 100000);
+    return () => clearInterval(interval);
+  }, []);
 
   let token = auth.token
   const todosUrl = `${URL_ENDPOINT}api/todos/`;
 
-  const fetcher = async (url, token) =>
-    fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then((res) =>
+  const fetcher = async (url, token) => 
+      fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then((res) =>
       res.json()
     );
+
+
+    
 
   // Fetch todos
   const { data: fetchedTodos, error: todosError } = useSWR([todosUrl, token], fetcher);
   if (todosError) <p>Loading failed...</p>;
   if (!fetchedTodos) <h1>Loading...</h1>;
-  console.log(fetchedTodos)
   
   
   const projectUrl = `${URL_ENDPOINT}api/projects/`;
